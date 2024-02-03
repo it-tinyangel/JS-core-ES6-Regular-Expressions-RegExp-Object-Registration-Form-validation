f21b45abfcafe82247bec7380c9fd7b4d0a4c4fb
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-02-03 02:05:46 +0200

    Updated js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const passwordField = document.getElementById('password');
-const togglePassword = document.querySelector('.password-toggle-icon');
+const iconTogglePassword = document.querySelector('.password-toggle-icon');
 
-togglePassword.addEventListener('click', toggle);
+iconTogglePassword.addEventListener('click', togglePassword);
 
 function showPssword() {
 	passwordField.setAttribute('type', 'text');
-	togglePassword.classList.remove('password-toggle-icon-hide');
-	togglePassword.classList.add('password-toggle-icon-show');
+	iconTogglePassword.classList.remove('password-toggle-icon-hide');
+	iconTogglePassword.classList.add('password-toggle-icon-show');
 }
 
 function hidePssword() {
 	passwordField.setAttribute('type', 'password');
-	togglePassword.classList.remove('password-toggle-icon-show');
-	togglePassword.classList.add('password-toggle-icon-hide');
+	iconTogglePassword.classList.remove('password-toggle-icon-show');
+	iconTogglePassword.classList.add('password-toggle-icon-hide');
 }
 
-function toggle() {
+function togglePassword() {
 	passwordField.type === 'password' ? showPssword() : hidePssword()
 }
