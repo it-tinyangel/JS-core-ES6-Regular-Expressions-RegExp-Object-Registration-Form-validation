4400c60bbfa68c016c5a63bc8fe80a83588b6e9d
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-02-03 21:44:54 +0200

    Updated js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-const inputPassword = document.getElementById('password');
+const form = document.querySelector('#signUpForm');
+const firstNameInput = document.querySelector('#firstName');
+const emailInput = document.querySelector('#email');
+const inputPassword = document.querySelector('#password');
 const iconTogglePassword = document.querySelector('.password-toggle-icon');
+const agreeCheckbox = document.querySelector('#agreeCheckbox');
+const signupButton = document.querySelector('#signUpButton');
+
+const successModal = document.querySelector('#successModal');
+const exploreButton = document.querySelector('#startButton');
 
 iconTogglePassword.addEventListener('click', togglePassword);
 
